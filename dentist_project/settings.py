64bde1c9fe2fcf3c4e4c59ccfb9341f7755b4450
SM-Saqib <syed.saqib@veeve.io>
2023-02-17 19:39:36 +0500

    change2

--- dentist_project/settings.py
+++ dentist_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dentist.apps.DentistConfig',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MIDDLEWARE = [
